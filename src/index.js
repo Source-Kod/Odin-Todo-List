74082f597c3f49270bd5fc3178c2222f06c54748
Source-Kod <Source-Kod@protonmail.com>
2023-08-20 23:19:33 -0500

    Add: Filter by DueDate list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { drawTaskList } from './modules/contentModule';
 import addDemoTask from './modules/demoTaskModule';
+import { drawFilterList } from './modules/filterModule';
 import { addEventsToFormButtons } from './modules/modalModule';
 import { drawProjectList } from './modules/projectListModule';
 
 addDemoTask();
 drawTaskList();
+drawFilterList();
 drawProjectList();
 addEventsToFormButtons();
--- src/modules/filterModule.js
+++ src/modules/filterModule.js
@@ -1,7 +1,9 @@
 import { isSameDay, isSameWeek, parseISO } from 'date-fns';
+import { drawTaskList } from './contentModule';
 
 // options are none, day, week
 let currentFilter = 'none';
+const filterListContainer = document.querySelector('#filter-list-container');
 
 function getCurrentFilter() {
   return currentFilter;
@@ -23,10 +25,59 @@ function filterArrayByProjectName(array, projectName) {
   return array.filter((ele) => ele.project === projectName);
 }
 
+function clearFilterContainer() {
+  while (filterListContainer.firstChild) {
+    filterListContainer.removeChild(filterListContainer.firstChild);
+  }
+}
+
+function drawFilterList() {
+  clearFilterContainer();
+
+  const noneFilter = document.createElement('button');
+  const dayFilter = document.createElement('button');
+  const weekFilter = document.createElement('button');
+
+  filterListContainer.appendChild(noneFilter);
+  filterListContainer.appendChild(dayFilter);
+  filterListContainer.appendChild(weekFilter);
+
+  noneFilter.textContent = 'None';
+  dayFilter.textContent = 'day';
+  weekFilter.textContent = 'Week';
+
+  noneFilter.classList = 'hover:text-sky-700';
+  dayFilter.classList = 'hover:text-sky-700';
+  weekFilter.classList = 'hover:text-sky-700';
+
+  if (currentFilter === 'none') noneFilter.classList.add('underline');
+  if (currentFilter === 'day') dayFilter.classList.add('underline');
+  if (currentFilter === 'week') weekFilter.classList.add('underline');
+
+  noneFilter.addEventListener('click', () => {
+    setCurrentFilter('none');
+    drawFilterList();
+    drawTaskList();
+  });
+
+  dayFilter.addEventListener('click', () => {
+    setCurrentFilter('day');
+    drawFilterList();
+    drawTaskList();
+  });
+
+  weekFilter.addEventListener('click', () => {
+    setCurrentFilter('week');
+    drawFilterList();
+    drawTaskList();
+  });
+}
+
 export {
   filterArrayByDay,
   filterArrayByWeek,
   filterArrayByProjectName,
   getCurrentFilter,
   setCurrentFilter,
+  drawFilterList,
 };
